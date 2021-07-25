b802dfeae97375a15065a18656b2d23d032b1cab
Vladislav Milushkov <radshura@yandex.ru>
2021-07-25 13:22:43 +0300

    6/7

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,45 @@
 import React from 'react';
+import {api} from '../utils/Api.js'
 
 function Main(props) {
+
+  const [userName, setUserName] = React.useState();
+  const [userDescription, setDescription] = React.useState();
+  const [userAvatar, setUserAvatar] = React.useState();
+  
+    React.useEffect(() => {
+      function initialUser() {
+        Promise.all([api.initialUsers()])
+            .then((result) => {
+              console.log(result)
+              setUserName(
+                result[0].name
+              );
+              setDescription(
+                result[0].about
+              );
+              setUserAvatar(
+                result[0].avatar
+              );
+            })
+            .catch((err) => {
+               console.log(err)
+            })
+      }
+      initialUser();
+    });
+    
 
   return (
     <main className="main">
         <section className="profile">
             <div className="profile-avatar">
-                <img id="avatar" className="profile-avatar__image" src="<%=require('./images/avatar.png')%>" alt='Аватарка' />
+                <img id="avatar" className="profile-avatar__image" src= {userAvatar} alt='Аватарка' />
                 <button onClick={props.onEditAvatar} type="button" className="profile-avatar__redact-button"></button>
             </div>
             <div className="profile-info">
-                <h1 id="name" className="profile-info__name">Жак-Ив Кусто</h1>
-                <p id="about" className="profile-info__job">Исследователь океана</p>
+                <h1 id="name" className="profile-info__name">{userName}</h1>
+                <p id="about" className="profile-info__job">{userDescription}</p>
                 <button onClick={props.onEditProfile} type="button" className="profile-info__edit-button"></button>
             </div>
             <button onClick={props.onAddPlace} type="button" className="profile__add-button"></button>
