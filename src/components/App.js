ec31adabecaab87c493cae8c2c912b46be7e6191
Vladislav Milushkov <radshura@yandex.ru>
2021-08-07 21:39:09 +0300

    5/5

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
-import api from '../utils/api.js';
+import api from '../utils/Apsi.js';
 import ImagePopup from './ImagePopup.js';
 import PopupWithForm from './PopupWithForm.js';
 import AddPlacePopup from './AddPlacePopup.js';
